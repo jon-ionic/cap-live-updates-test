c2160dabbbd482854c2d0de313a3a480f39a287c
Jon <jon.herman@ionic.io>
2023-02-23 10:26:31 -0500

    add live updates plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,16 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'cap-live-updates-test',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    LiveUpdates: {
+      appId: 'b7a8b20a',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2,
+      key: 'ionic_cloud_public.pem'
+    }
+  }
 };
 
 export default config;
