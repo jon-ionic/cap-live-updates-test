799564e888d66c0980b13542f9e9e0f05ebf0d92
Jon <jon.herman@ionic.io>
2023-02-23 10:35:30 -0500

    remove live updates plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.jon',
   appName: 'cap-live-updates-test',
   webDir: 'www',
   bundledWebRuntime: false,
-  plugins: {
-    LiveUpdates: {
-      appId: 'b7a8b20a',
-      channel: 'Production',
-      autoUpdateMethod: 'background',
-      maxVersions: 2,
-      key: 'ionic_cloud_public.pem'
-    }
-  }
 };
 
 export default config;
